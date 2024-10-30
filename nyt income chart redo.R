0dab09c4c67288ab0a4d4e6aff2f90791d4cd6a8
greg-dubrow <dubrowg@gmail.com>
2024-10-30 20:18:55 +0100

    figuring out how to extract & analyse things

--- nyt income chart redo.R
+++ nyt income chart redo.R
@@ -23,15 +23,116 @@
 # use ACS via ipums?
 
 library(ipumsr)
+library(srvyr)
+library(tidyverse)
+library(tidylog)
+library(janitor)
+library(DataExplorer)
 
-ipums_data_collections()
+# metadata
+nytddi <- read_ipums_ddi("data/usa_00004.xml")
+names(nytddi)
 
-# to get variable names
-# data for ACS https://usa.ipums.org/usa-action/variables/group
+# display variable information in console
+nytddi$var_info$code_instr[7] %>%
+  strwrap(60)
+
+# opens up object in viewer to explore variables, values and labels
+ipums_view(nytddi)
 
 
+# display values and labels for variables
+nytddi$var_info[[4]][[14]]
 
+
+# actual data
+nytdata <- read_ipums_micro(nytddi)
+glimpse(nytdata)
+
+nytdata %>%
+  count(YEAR)
+
+nytdata %>%
+  filter(YEAR == 1980) %>%
+  count(EDUC) %>%
+  print(n = 49)
+
+nytdata %>%
+  count(YEAR, RACE) %>%
+  print(n = 49)
+
+summary(nytdata$INCTOT)
+summary(nytdata$INCWAGE)
+
+## create dfs for each year, then do summary stats by category. create perm dfs for each year to join
+  ## into longtidunal df
+## as each year frame being made, might need to delete full file and reload
 ## analysis - use survey and/or srvry packages
 # https://federicovegetti.github.io/teaching/heidelberg_2018/lab/sst_lab_day2.html
 ## Use PERWT for person-specific variables, HHWT for household variables
-library(srvyr)
+# also good explainer at:
+  # https://coreysparks.github.io/appdem_Book/survey.html#replicates-and-jack-knifes-and-expansions-oh-my
+
+## 1980 - note, ed attainment does not distinguish beyond bac degree. that starts in 1990
+nytdata_1980 <- nytdata %>%
+  filter(YEAR == 1980) %>%
+  select(-MULTYEAR, -SERIAL, - CBSERIAL, -EDUCD, -HISPAND, -GQ, -RACED, -HISPAND, -SAMPLE) %>%
+  # recode race/ethnicity. note two or more does not come until 2000
+  mutate(eth_cat = case_when((RACE == 1 & HISPAN == 0) ~ "White",
+                             (RACE == 2 & HISPAN == 0) ~ "African American",
+                             (RACE == 3 & HISPAN == 0) ~ "Native American",
+                             (RACE %in% c(4, 5, 6) & HISPAN == 0) ~ "Asian/PI",
+                             (RACE = 7 & HISPAN == 0) ~ "Other (not defined)",
+                             (RACE %in% c(8, 9) & HISPAN == 0) ~ "Two or more",
+                             HISPAN >=1 ~ "Hispanic/Latino")) %>%
+  mutate(eth_cat = factor(eth_cat, levels = c("Native American", "African American",
+                                              "Hispanic/Latino", "Asian/PI",
+                                              "White", "Two or more", "Other (not defined)"))) %>%
+  # recode education - note, no coding for grad/professional degs in 1980
+  mutate(ed_cat = case_when(EDUC <6 ~ "Less than HS",
+                            EDUC == 6 ~ "HS Diploma",
+                            between(EDUC, 7, 9) ~ "Some College",
+                            EDUC >= 10 ~ "BA+")) %>%
+  mutate(ed_cat = factor(ed_cat, levels = c("Less than HS", "HS Diploma", "Some College", "BA+")))
+
+glimpse(nytdata_1980)
+
+nytdata_1980 %>%
+  count(eth_cat)
+
+nytdata_1980 %>%
+  count(ed_cat, EDUC)
+
+nytdata_1980 %>%
+  count(SEX, ed_cat)
+
+nytdata_1980 %>%
+  summarise(tot_pop = sum(PERWT), n_resp = n())
+
+# run ed attain by sex with std error
+nytdata_1980_edsex <-
+nytdata_1980 %>%
+  as_survey_design(cluster = CLUSTER, strata = STRATA, weights = PERWT) %>%
+  group_by(SEX, ed_cat) %>%
+  summarise(sex_ed = survey_total(vartype = c("se", "cv"))) %>%
+  ungroup() %>%
+  group_by(SEX) %>%
+  mutate(sex_ed_pct = sex_ed /sum(sex_ed)) %>%
+  ungroup()
+
+
+#nytdata_1980_edsex_inc <-
+  nytdata_1980 %>%
+  mutate(inctot2 = as.numeric(INCTOT)) %>%
+  as_survey_design(cluster = CLUSTER, strata = STRATA, weights = PERWT) %>%
+  group_by(SEX, ed_cat) %>%
+  summarise(sex_ed_inctot = survey_median(inctot2, na.rm = T))
+
+
+
+nytdata_1980_sd <-  nytdata_1980 %>%
+  as_survey_design(cluster = CLUSTER, strata = STRATA, weights = PERWT)
+
+
+nytdata_1980 %>%
+  count(RACE)
